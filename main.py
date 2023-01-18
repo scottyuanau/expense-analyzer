3e5e4c99beaae4f818bd46b4ffab1b36c90e9c4f
Scott Yuan <scoworthyuan@gmail.com>
2023-01-18 20:43:43 +1100

    optimized output report to integrate more insights.

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ import os
 original_df = pd.read_csv('input.csv')
 
 # ask user to input the start and end date for the report
-start = input('What is the start date? format: DDMMYYYY\n')
-end = input('What is the end date? format: DDMMYYYY\n')
-start_date = f'{start[4:]}-{start[2:4]}-{start[0:2]}'
-end_date = f'{end[4:]}-{end[2:4]}-{end[0:2]}'
+# start = input('What is the start date? format: DDMMYYYY\n')
+# end = input('What is the end date? format: DDMMYYYY\n')
+# start_date = f'{start[4:]}-{start[2:4]}-{start[0:2]}'
+# end_date = f'{end[4:]}-{end[2:4]}-{end[0:2]}'
 
-# start_date = '2021-01-01'
-# end_date = '2022-12-31'
+start_date = '2021-01-01'
+end_date = '2022-12-31'
 
 original_df['Date'] = pd.to_datetime(original_df['Date'], format='%d/%m/%Y')
 
@@ -32,7 +32,7 @@ if not os.path.exists('results'):
 r = relativedelta.relativedelta(datetime.strptime(end_date, '%Y-%m-%d'), datetime.strptime(start_date, '%Y-%m-%d'))
 total_period = (r.years * 12) + r.months
 
-# Todo: specify the supplier column
+
 suppliers = df['Narrative']
 
 each_supplier_cost_all = df.groupby(['Narrative'])['Debit Amount'].sum()  # group supplier by name
@@ -82,7 +82,16 @@ yticks.append(other_amount)
 analyzed_df = pd.DataFrame({'Labels': labels, 'Expenses': sizes}).sort_values(by='Expenses', ascending=False)
 
 
-fig1, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10), dpi=150)
+fig1, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 15), dpi=150)
+fig1.suptitle(f'Expense Analysis from {start_date} to {end_date}', size=20,weight='bold')
+fig1.tight_layout(pad=5)
+# set the spacing between subplots
+plt.subplots_adjust(left=0.1,
+                    bottom=0.1,
+                    right=0.9,
+                    top=0.9,
+                    wspace=0.4,
+                    hspace=0.4)
 # pie chart
 
 # set up explode for the pie chart
@@ -95,19 +104,27 @@ for value in analyzed_df['Expenses']:
 
 
 ax1.pie(analyzed_df['Expenses'], labels=analyzed_df['Labels'], autopct='%1.1f%%', explode=explode, startangle=90)
+ax1.set_title('Percentage Analysis',fontdict={'size': 16, 'weight': 'bold'})
 ax1.axis('equal')
-ax1.set_title('Expense Percentage', fontdict={'size': 20, 'weight': 'bold'})
 ax1.legend()
 
-# bar chart
+# bar chart, top 5 categories
 ax2.bar(analyzed_df['Labels'], analyzed_df['Expenses'] / 100 * total_living_expense)
 ax2.set_xlabel('Expense Category')
-ax2.set_ylabel('Expense per month')
-ax2.set_title('Category Spending per Month', fontdict={'size': 20, 'weight': 'bold'})
+ax2.set_ylabel('Expense per month', labelpad=10)
+ax2.set_title('Top Spending Categories', fontdict={'size': 16, 'weight': 'bold'})
 ax2.set_yticks(np.arange(0, 1100, 100))
 ax2.yaxis.set_major_formatter('${x:1.0f}')  # add dollar sign to y axis
-plt.savefig(f'results/result-chart.png', dpi=300)
 
-analyzed_result.nlargest(n=5).to_csv('results/top-5-categories-spending.csv')
-living_expense_df.nlargest(n=10, columns="Monthly Expenses").to_csv('results/top-10-expenses.csv', index=False)
+# bar chart, top 10 expenses
+ax3.bar(living_expense_df.nlargest(n=10, columns="Monthly Expenses")['Supplier'], living_expense_df.nlargest(n=10, columns="Monthly Expenses")['Monthly Expenses'], width=0.5)
+ax3_x_labels = ax3.get_xticklabels()
+ax3_truncated_x_labels =[label.get_text()[:10] for label in ax3_x_labels]
+ax3.set_xticklabels(ax3_truncated_x_labels, rotation=45)
+ax3.set_title('Top Spending Suppliers', fontdict={'size': 16, 'weight': 'bold'})
+
+
+plt.savefig(f'results/report.pdf', dpi=300)
+
+
 print('Reports exported.')
